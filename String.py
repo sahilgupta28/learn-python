1d3fdf236626ffb37327daf94a44b6d973b424cd
Sahil Gupta <er.sahilgupta1@gmaiil.com>
2019-12-11 11:37:05 +0530

    learn about string

---
+++ String.py
@@ -0,0 +1,12 @@
+#Strint
+
+str = 'How are you?' #use '' for strint
+str1 = "I'm fine" # use "" for add ' in result
+str2 = 'Yor\'re ok?' # It will also work
+str3 = """use 3 colon to  
+add mutiple line 
+text."""
+print(str)
+print(str1)
+print(str2)
+print(str3)
