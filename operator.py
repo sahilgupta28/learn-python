c7b7e82bbdf6f440760136289d53fa64d2b8f62c
Sahil Gupta <er.sahilgupta1@gmaiil.com>
2019-12-11 11:20:11 +0530

    oerator basic

---
+++ operator.py
@@ -0,0 +1,19 @@
+number = 1_000_000 #underscore can use to increase readebility of numbers.
+print(number)
+
+age = '20'
+print(int(age)+1) #int() convert any datatype to integer if it's a valid integer
+
+
+total = '20.06'
+print(float(total)+1) #float() convert any datatype to float if it's a valid float
+
+# + - * / % operator are similar with other languages
+
+print (5//10) #return only integer part of 5/10
+
+print(5**2) #return power
+
+print("-"*90) #Print strint 90 times
+
+print('hello '+'world') #concatination
